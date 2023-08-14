211ba9b51cfdffae7d505147737ecc1b46748bd1
Cyber-Gen <78339004+Cyber-Gen@users.noreply.github.com>
2023-08-13 19:18:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import click
+from sysit.commands.cli import cli
+# from sysit.common_imports import *
+
+def main():
+    """Main entry point for the application script"""
+    click.echo("sysit is running... \n")
+
+    # add any initalization code here
+
+    #run the Click-based CLI commands defined in cli.py
+    cli()
+
+if __name__ == "__main__":
+    main()
+
