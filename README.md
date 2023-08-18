9696b8aa83768e48a9fefb236e456def6d580811
Cyber-Gen <78339004+Cyber-Gen@users.noreply.github.com>
2023-08-18 06:14:36 -0500

    more clean up and README file added

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+
+# NOTE: THIS PROGRAM IS A WORK IN PROGRESS AND STILL IN DEVELOPMENT
+
+# Overview
+
+`sysit` is python program with easy to use CLI that allows you to run some system and network checks. It is capable of doing application and system vulnerablity tests and generate a report.
+
+## Prerequisites
+
+Before using `sysit`, you will need to have the following installed and configured:
+
+- `Python 3.x`
+- `pip`
+- `Git`
+
 # sysit
+
+`sysit` uses Click CLI to parse command line arguments and execute the appropriate command.
+
+## Usage
+
+1. Install the program using setup.py:
+
+        python setup.py
+
+2. Use the program via CLI:
+
+        sysit [OPTIONS] COMMAND [ARGS]...
--- setup.py
+++ setup.py
@@ -1,31 +1,79 @@
 import subprocess
 import os
 import sys
+from pkg_resources import get_distribution, DistributionNotFound
 
-# Read required packages and modules from requirements.txt
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-# Check if required packages are installed
-installed = subprocess.check_output(['pip', 'freeze'])
-installed = [pkg.decode().split('==')[0] for pkg in installed.split()]
-
-# Install missing packages
-missing = [pkg for pkg in required if pkg.split('==')[0] not in installed]
-if missing:
-    subprocess.call(['pip', 'install'] + missing)
 
 # Check Python version
-with open('requirements.txt') as f:
-    for line in f:
-        if line.startswith('python=='):
-            required_version = line.split('==')[1]
-            if not (required_version.startswith('2') and sys.version_info.major == 2) and not (required_version.startswith('3') and sys.version_info.major == 3):
-                raise ValueError(f"Python version {required_version} is required, but you are running Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
+if sys.version_info < (3, 0):
+    print("This program requires Python version 3 or greater.")
+    print("\nSetup failed. Please install the required version of Python and try again.")
+    sys.exit(1)
+
+# Read required packages and modules from requirements.txt
+try:
+    with open('requirements.txt') as f:
+        required = [line.strip() for line in f if line.strip()]
+except FileNotFoundError:
+    print("\nError: requirements.txt file not found.")
+    print("\nSetup failed.")
+    sys.exit(1)
+
+# Check if required packages are installed
+installed = []
+for pkg in required:
+    try:
+        get_distribution(pkg.split('==')[0])
+        installed.append(pkg)
+    except DistributionNotFound:
+        print("\nDistribution Not Found")
+        print("\nSetup failed.")
+        sys.exit(1)
+
+# Install missing packages
+missing = [pkg for pkg in required if pkg not in installed]
+if missing:
+    print(f"\nInstalling missing packages: {', '.join(missing)}")
+    try:
+        subprocess.call(['pip', 'install'] + missing)
+    except subprocess.CalledProcessError:
+        print("\nError: failed to install missing packages.")
+        print("\nSetup failed.")
+        sys.exit(1)
+
 
 # Create folder for program installation
 if not os.path.exists('sysit'):
-    os.makedirs('sysit')
+    try:
+        os.makedirs('sysit')
+    except OSError:
+        print("\nError: failed to create folder for program installation.")
+        print("\nSetup failed.")
+        sys.exit(1)
 
-# Clone additional GitHub repository
-subprocess.call(['git', 'clone', 'https://github.com/CVEProject/cvelistV5.git', 'sysit/repo'])
+if not os.path.exists('sysit/repos'):
+    try:
+        os.makedirs('sysit/repos')
+    except OSError:
+        print("\nError: failed to create folder for program installation.")
+        print("\nSetup failed.")
+        sys.exit(1)
+
+if not os.path.exists('sysit/reports'):
+    try:
+        os.makedirs('sysit/reports')
+    except OSError:
+        print("\nError: failed to create folder for program installation.")
+        print("\nSetup failed.")
+        sys.exit(1)
+
+if not os.path.exists('sysit/repos/cvelistV5'):
+    try:
+        subprocess.call(['git', 'clone', 'https://github.com/CVEProject/cvelistV5.git', 'sysit/repos/cvelistV5'])
+    except OSError:
+        print("\nError: failed to create folder for program installation.")
+        print("\nSetup failed.")
+        sys.exit(1)
+
+print("\nEverything looks good.\nSetup complete!")
+
